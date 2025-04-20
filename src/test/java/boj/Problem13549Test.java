b571a4d934512f031f0836a2cb62f7669b0f2851
crimy <tmddnjs950@gmail.com>
2025-04-20 22:46:07 +0900

    13549. 숨바꼭질

---
+++ src/test/java/boj/Problem13549Test.java
@@ -0,0 +1,12 @@
+package boj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Problem13549Test {
+    @Test
+    void testSolve() {
+        assertEquals(2, Problem13549.solve(5, 17));
+    }
+}
